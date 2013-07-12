0550a3fed62331275d789ff719369e5eed8f6aa5
osktgui <osktgui@gmail.com>
2013-07-11 22:45:00 -0500

    Initial Commit

---
+++ LoginWithFacebook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LoginWithFacebook
+//
+//  Created by mac developer on 7/11/13.
+//  Copyright (c) 2013 mac developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
